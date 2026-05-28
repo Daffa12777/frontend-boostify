828a610c1633ef21e5ea0b84b1bc67d60ee13fb8
Daffa12777 <daffafr05@gmail.com>
2026-05-28 14:55:00 +0700

    newww

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -20,7 +20,7 @@ export default NextAuth({
 
       async authorize(credentials) {
         try {
-          const res = await fetch('http://localhost:3000/api/auth/login', {
+          const res = await fetch('https://web-boostify.vercel.app/api/auth/login', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
